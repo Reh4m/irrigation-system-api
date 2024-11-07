16d13b711250ff0096ee2aeae45970c14b5d023f
emmarp <ruiizemmanuel@gmail.com>
2024-11-07 02:07:45 -0600

    DB Entities loaded into TypeORM DataSource: Fix DataSource is not set

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,13 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+import { join } from "path";
+
 import {
   MYSQL_DB,
   MYSQL_HOST,
   MYSQL_USER,
   MYSQL_PASSWORD,
   MYSQL_PORT,
-} from "../config";
-import { DataSource, DataSourceOptions } from "typeorm";
+} from "@config";
 
 const dbConfig: DataSourceOptions = {
   type: "mysql",
@@ -14,8 +16,11 @@ const dbConfig: DataSourceOptions = {
   username: MYSQL_USER,
   password: MYSQL_PASSWORD,
   database: MYSQL_DB,
-  logging: true,
+  logging: false,
   synchronize: true,
+  entities: [join(__dirname, "../**/*.entity{.ts,.js}")],
+  migrations: [join(__dirname, "../**/*.migration{.ts,.js}")],
+  subscribers: [join(__dirname, "../**/*.subscriber{.ts,.js}")],
 };
 
 export const dbConnection = async () => {
